0b903b0e3f6f7e7a8f7d07730810c5da6d710b45
Juliant <iqbaljulian@live.com>
2021-03-24 01:04:02 +0700

    fixin some typos

--- week2/3.js
+++ week2/3.js
@@ -2,7 +2,7 @@ const seleksiNilai = (awal, akhir, nilai) => {
   if(typeof awal != 'number' || typeof akhir != 'number' || nilai.length === 0) {
     return console.log(`Pastikan nilai, nilai awal dan nilai akhir terisi dengan benar`)
   } else if(awal > akhir) {
-    console.log(`Nilai akhir harus lebih besar dari nilai akhir`);
+    console.log(`Nilai akhir harus lebih besar dari nilai awal`);
   } else {
     let temp = []
     for(let i in nilai) {
@@ -15,5 +15,7 @@ const seleksiNilai = (awal, akhir, nilai) => {
   }
 }
 
-seleksiNilai(1, 20 , [])
+seleksiNilai(20, 9 , [19, 20, 1, 2, 4])
+// []
 seleksiNilai(60, 99, [10, 20, 30, 60, 80, 50, 88, 69, 89, 100, 90, 72]);
+// [60, 69, 72, 80, 88, 89, 90]
